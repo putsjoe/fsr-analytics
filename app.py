5892e5be8719078fe3f0ae9f6f29a622bb02305c
Joe <joe92v2@gmail.com>
2020-06-16 00:13:03 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,77 @@
+
+import json
+import datetime
+
+import redis
+from flask import Flask, render_template, request, abort, make_response
+from flask_basicauth import BasicAuth
+from user_agents import parse as parse_agent
+
+
+# NEXT: Set this up on bethink behind a subdomain, see if you can get the ip address of accesser. Also test on mobile.
+
+app = Flask(
+    __name__, static_url_path='', static_folder='static',
+    template_folder='templates')
+app.secret_key = b'_5#y2L"sjjw8sdfds27F4sd8z\n\xec]/'
+
+REDIS_STREAM_NAME = 'analytics'
+COOKIE_NAME = 'analytics-tracker'
+
+app.config['BASIC_AUTH_USERNAME'] = 'admin'
+app.config['BASIC_AUTH_PASSWORD'] = 'analytics'
+basic_auth = BasicAuth(app)
+
+r = redis.Redis(decode_responses=True)
+
+print(f'Using redis stream name {REDIS_STREAM_NAME}')
+
+
+@app.route('/noscript.gif')
+def serve():
+    if not request.referrer:
+        return abort(404)
+    r.xadd(REDIS_STREAM_NAME, {
+        'args': json.dumps(request.args),
+        'remote_addr': str(request.environ.get('HTTP_X_REAL_IP', request.remote_addr)),
+        'user_agent': str(parse_agent(request.user_agent.string)),
+        'referrer': str(request.referrer),
+    })
+    response = make_response(app.send_static_file('noscript.gif'))
+    # Dont allow the gif to be cached, ensuring all pageviews are recorded.
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    return response
+
+
+@app.route('/analytics.js')
+def servejs():
+    return app.send_static_file('a.js')
+
+
+@app.route('/a')
+def server():
+    print(request.args)
+
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
+
+@app.route('/')
+@basic_auth.required
+def home():
+    alldata = r.xrevrange(REDIS_STREAM_NAME)
+    data = []
+    for d in alldata:
+        data.append([
+            d[0],
+            redis.client.timestamp_to_datetime(int(d[0][:10])),
+            d[1].get('remote_addr'),
+            d[1].get('user_agent'),
+            d[1].get('referrer'),
+            d[1].get('args'),
+        ])
+
+    return render_template('index.html', data=data)
---
+++ static/a.js
@@ -0,0 +1,22 @@
+var tracker_url = 'http://localhost:5000/noscript.gif'
+function tracker(event) {
+    var t = 'tracker'
+    var extra = '?referrer=' + window.document.referrer
+    if (event) {
+        extra += '&event=' + event 
+    }
+    if (window.localStorage.getItem(t)) {
+        extra += '&last=' + window.localStorage.getItem(t)
+        window.localStorage.setItem(t, Date.now())
+    } else {
+        window.localStorage.setItem(t, Date.now())
+        extra += '&unique=true'
+    }
+    var src = tracker_url + extra,
+        img = document.createElement('img')
+    img.src = src;
+    document.body.appendChild(img).remove()
+}
+window.onload = function() {
+    tracker()
+}
